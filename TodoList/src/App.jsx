63986edd19b99c2452f41b40d898c5131b5f7cdf
Bett Enock <bettenock968@gmail.com>
2023-08-26 21:32:30 -0700

    Add main component page.

---
+++ TodoList/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="8xl font-bold text-blue-400 text-center underline">Bett Enock</h1>
+    </>
+  )
+}
+
+export default App
